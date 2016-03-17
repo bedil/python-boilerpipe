ab80209b24e9b142f715008babce1a75ed743604
Sharjeel Imam <sharjeel.imam@gmail.com>
2016-03-18 03:58:48 +0530

    updated for Python 3.5

--- src/boilerpipe/extract/__init__.py
+++ src/boilerpipe/extract/__init__.py
@@ -1,5 +1,5 @@
 import jpype
-import urllib2
+import urllib
 import socket
 import charade
 import threading
@@ -38,11 +38,11 @@ class Extractor(object):
             encoding    = connection.headers['content-type'].lower().split('charset=')[-1]
             if encoding.lower() == 'text/html':
                 encoding = charade.detect(self.data)['encoding']
-            self.data = unicode(self.data, encoding)
+            self.data = str(self.data, encoding)
         elif kwargs.get('html'):
             self.data = kwargs['html']
-            if not isinstance(self.data, unicode):
-                self.data = unicode(self.data, charade.detect(self.data)['encoding'])
+            if not isinstance(self.data, str):
+                self.data = str(self.data, charade.detect(self.data)['encoding'])
         else:
             raise Exception('No text or url provided')
 
